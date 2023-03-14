b0fc32bd04645273f317d03e50f0bccbff481d35
Hristo <hristo.anastasov@gmail.com>
2023-03-14 17:08:05 +0200

    Task-Management-App updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.com
 import { ErrorRoutingModule } from './error-routing/error-routing.module';
 import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
 import { ListViewComponent } from './list-view/list-view.component';
+import { MasterViewComponent } from './master-view/master-view.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'kanban-board', pathMatch: 'full' },
   { path: 'error', component: UncaughtErrorComponent },
   { path: 'kanban-board', component: KanbanBoardComponent, data: { text: 'Kanban Board' } },
   { path: 'list-view', component: ListViewComponent, data: { text: 'List View' } },
+  { path: 'master-view', component: MasterViewComponent, data: { text: 'Master View' } },
   { path: '**', component: PageNotFoundComponent } // must always be last
 ];
 
